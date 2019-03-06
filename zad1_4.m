167d9bf50d411821c5839530c79323b9758c2ab9
wilqqq <wilk.michal96@gmail.com>
2019-03-06 19:20:39 +0100

    policzone unbiased, biased

--- zad1_4.m
+++ zad1_4.m
@@ -12,17 +12,26 @@ x2=horzcat(x, zeros(1,N)); % dodanie N zer za wektorem funkcji
 %2 razy mniej obliczeñ hehehe
 y1 = [x zeros(1,N)];
 kor_wilq = zeros(1,2*N-1);
+kor_unbiased = kor_wilq;
+
 for k = 0:N-1
    y_mv = [zeros(1,k) y1(1:end-k)];
    kor_wilq(k+N) = sum(y_mv.*y1);
+   kor_unbiased(k+N) = kor_wilq(k+N)/(N-k);
 end
+
 kor_wilq(1:N-1) = fliplr(kor_wilq(N+1:2*N-1));
+kor_unbiased(1:N-1) = fliplr(kor_unbiased(N+1:2*N-1));
 
 for k=1:(2*N-1)
         x2_mv=horzcat(zeros(1,k), x2(1:end-k));% przesuwanie funkcji x2
         kor(k)=sum(x1.*x2_mv); %sumowanie wszystkich iloczynów przesuniêtej 
         % funkcji x2 oraz x1 
 end
+
+kor = kor_wilq; 
+kor_biased = kor./N;
+
 t2=[1:length(kor)]*1/fs-1;
 %tworzenie wykresu
 plot(t2,kor,':','LineWidth',2)
