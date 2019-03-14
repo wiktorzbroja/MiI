70e32e26f4a53897d129950079ce98820a3345f6
wzbroja <wiktorzbroja@gmail.com>
2019-03-14 21:22:44 +0100

    działamy

--- Lab #2/zad1.m
+++ Lab #2/zad1.m
@@ -4,18 +4,23 @@ clear all
 [x, fs]=audioread('whitenoise2018.wav');
 [y, fs]=audioread('whitenoise_rec2018.wav');
 
+
 Rxx= xcorr(x,x);
 Rxy= xcorr(x,y);
 
+t=(-length(Rxx)/2:length(Rxx)/2-1)/fs;%wektor czasu w Hz
 subplot(211)
-plot((-length(Rxx)/2:length(Rxx)/2-1)/fs,Rxx)
-axis tight
-xlabel('time in [s]')
-ylabel('Rxx')
-title( 'Autocorrelation Rxx')
+set(gcf,'color','w');
+plot(t,Rxx)
+axis tight %ustawienie limitu osi do zakresu danych
+%axis([-100 100 min(Rxx) max(Rxx)])
+xlabel('Czas [s]')
+ylabel('Rxx [j^2]')
+title( 'Autokorelacja Rxx')
 subplot(212)
-plot((-length(Rxy)/2:length(Rxy)/2-1)/fs,Rxy)
+plot(t,Rxy)
 axis tight
-xlabel('time in [s]')
-ylabel('Rxy')
-title( 'Crosscorrelation Rxy')
+%axis([-100 100 min(Rxy) max(Rxy)])
+xlabel('Czas [s]')
+ylabel('Rxy [j^2]')
+title( 'Korelacja Rxy')
