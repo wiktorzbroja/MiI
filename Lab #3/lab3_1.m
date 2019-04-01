a7e7ded14a5bdf3e209b75e42d8d3ff1510410ae
wzbroja <wiktorzbroja@gmail.com>
2019-04-02 00:00:08 +0200

    Sprawko

--- Lab #3/lab3_1.m
+++ Lab #3/lab3_1.m
@@ -1,13 +1,28 @@
-%[wn,fs]=audioread('WhiteNoise2018.wav');
-%[hn, fs]=audioread('HarmNoise2018.wav');
+[wn,fs]=audioread('WhiteNoise2018.wav');
+[hn, fs]=audioread('HarmNoise2018.wav');
 subplot(211)
+set(gcf,'color','w');
 periodogram(wn,[],[],fs)
+title('Gêstoœæ widmowa mocy szumu bia³ego - Periodogram')
+xlabel('Czêstotliwoœæ [kHz]')
+ylabel('Gêstoœæ widmowa mocy [dB/Hz]')
+
 subplot(212)
 periodogram(hn,[],[],fs)
+title('Gêstoœæ widmowa mocy szumu bia³ego i sinusoidy - Periodogram')
+xlabel('Czêstotliwoœæ [kHz]')
+ylabel('Gêstoœæ widmowa mocy [dB/Hz]')
 
 figure(2)
-
+set(gcf,'color','w');
 subplot(211)
 pwelch(wn,[],[],[],fs)
+title('Gêstoœæ widmowa mocy szumu bia³ego - met. Welcha')
+xlabel('Czêstotliwoœæ [kHz]')
+ylabel('Gêstoœæ widmowa mocy [dB/Hz]')
+
 subplot(212)
 pwelch(hn,[],[],[],fs)
+title('Gêstoœæ widmowa mocy szumu bia³ego i sinusoidy - met. Welcha')
+xlabel('Czêstotliwoœæ [kHz]')
+ylabel('Gêstoœæ widmowa mocy [dB/Hz]')
--- Lab #3/lab3_2.m
+++ Lab #3/lab3_2.m
@@ -1,34 +1,48 @@
-%[lpc_n,fs]=audioread('LPC_noise2018.wav');
+[lpc_n,fs]=audioread('LPC_noise2018.wav');
 figure (1)
+set(gcf,'color','w');
 subplot(211)
 periodogram(lpc_n,[],[],fs)
+title('Gêstoœæ widmowa mocy sygna³u - Periodogram')
+xlabel('Czêstotliwoœæ [kHz]')
+ylabel('Gêstoœæ widmowa mocy [dB/Hz]')
+
 subplot(212)
 pwelch(lpc_n,[],[],[],fs)
+title('Gêstoœæ widmowa mocy sygna³u - met. Welcha')
+xlabel('Czêstotliwoœæ [kHz]')
+ylabel('Gêstoœæ widmowa mocy [dB/Hz]')
 
+figure (2)
 p=13;
 [a,g] = lpc(lpc_n,p);
 est_x= filter([0 -a(2:end)],1,lpc_n);
 e = lpc_n -est_x;
 [acs,lags] = xcorr(e,'coeff');
-figure (2)
+set(gcf,'color','w');
 subplot(211)
 plot(lags,acs), grid
-title 'Autocorrelation of the Prediction Error'
-xlabel 'Lags', ylabel 'Normalized value'
+title 'Autokorelacja b³êdu predykcji'
+xlabel 'OpóŸnienie', ylabel 'Wartoœæ znormalizowana'
+
 subplot(212)
 yi = randn(length(lpc_n),1);
 [acs2,lags] = xcorr(yi,'coeff');
 plot(lags,acs2)
+title 'Autokorelacja szumu bia³ego'
+xlabel 'OpóŸnienie', ylabel 'Wartoœæ znormalizowana'
 
 figure (3)
+set(gcf,'color','w');
 [h,w] = freqz(1,a);
 plot(w/pi*fs/2*1e-3,20*log10(abs(h)))
 grid
-ylabel('Magnitude (dB)')
-xlabel('Frequency in kHz')
-title('Estimated spectrum')
+ylabel('Amplituda (dB)')
+xlabel('Czêstotliwoœæ [kHz]')
+title('OdpowiedŸ impulsowa odwróconego filtru wybielaj¹cego')
 
 figure(4)
+set(gcf,'color','w');
 X = fft(lpc_n)
 plot(20.*log10(abs(X(1:length(X)/2))))
 grid on
