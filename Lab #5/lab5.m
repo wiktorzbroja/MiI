d90970ab0549fa2a4a55a135cfed7e8859e4f103
wilqqq <wilk.michal96@gmail.com>
2019-05-12 17:57:27 +0200

    happap

--- Lab #5/lab5.m
+++ Lab #5/lab5.m
@@ -1,5 +1,8 @@
 load Pulse
 [Y,fs]=audioread('Nabucco.wav');
+y1=Y(:,1);
+y2=Y(:,2);
+
 WINDOW=4000
 NOVERLAP=2000
 NFFT=5000
@@ -8,23 +11,31 @@ spectrogram(yp,WINDOW,NOVERLAP,NFFT,fs)
 figure(2)
 periodogram(yp,[],[],fs)
 
-
-figure(3)
+%%tworzenie filtru
+% sptool -> bandpass 0.02 0.08 0.164 0.224
  [b,a] = tfdata(filt1,'v');
 % sound(Y,fs)
  z = filter(b,a,Y);
  sound(z,fs)
-
-y1=Y(:,1);
-y2=Y(:,2);
-subplot(211)
-plot(y1)
-subplot(212)
-plot(y2,'r')
-figure(4)
+ 
+%% filtrowanko
 z1 = filter(b,a,y1);
 z2 = filter(b,a,y2);
-subplot(211)
+
+%% wykresy czasowe przed i po filtrowanku
+figure(3)
+subplot(221)
+plot(y1)
+subplot(222)
+plot(y2,'r')
+subplot(223)
 plot(z1)
-subplot(212)
+subplot(224)
 plot(z2,'r')
+
+%% spektrogramy kana³ów po filtrowanku
+figure(4)
+subplot(211)
+periodogram(z1,[],[],fs)
+subplot(212)
+periodogram(z2,[],[],fs)
